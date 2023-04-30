32616156ed967ed07345148c75fd1915385d5a6c
Patrick-Ngige <wakemanjajr@gmail.com>
2023-04-30 14:29:35 +0300

    adding the single.php code

--- wp-content/themes/assessment4/content-blogs.php
+++ wp-content/themes/assessment4/content-blogs.php
@@ -1,5 +1,5 @@
 
 
-<h2><?php the_title(); ?></h2>
+<h3><?php the_title(sprintf('<h3 class="entry-title"><a href= "%s">', esc_url(get_permalink() )), '</a></h3>'); ?></h3>
 <small>Posted on: <?php the_time('F j, Y');?>&nbsp;<?php the_time('g:i a') ?></small>
 <?php the_content();?> 
--- wp-content/themes/assessment4/single.php
+++ wp-content/themes/assessment4/single.php
@@ -1,19 +1,24 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
-<?php 
-if(have_posts()):
-    while(have_posts()): the_post();
-    ?>
-    <h2><?php the_title(); if(current_user_can('manage_options')){ echo '||';
-            edit_post_link();} ?> </h2>
-       <?php     endwhile;
-            endif;
-            ?>
+<?php
+if (have_posts()) :
+    while (have_posts()) : the_post();
+?>
+        <header>
+            <h3><?php the_title();  ?></h3>
+        </header>
+        <?php the_content(); ?>
+
+
+
+<?php endwhile;
+endif;
+?>
 
 
 <!-- ACTIVATING THE COMMENTS -->
-<?php 
-if(comments_open()){
+<?php
+if (comments_open()) {
     comments_template();
 }
 ?>
